5bd1cf7cf06b7fc4590cb9637cee031a20e68231
yasmineimane <mesbahi.imane.yasmine@gmail.com>
2024-03-27 14:42:08 -0700

    0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
-/*
- * binary_tree_node - Creates a binary tree node
+/**
+ * binary_tree_node - creates a binary tree node
  *
  * @parent: pointer to the parent node of the node to create
  * @value: the value to put in the new node.
