fb1fa727f4a348e5756de5847c1adc29090dc526
yasmineimane <mesbahi.imane.yasmine@gmail.com>
2024-03-27 15:04:16 -0700

    Insert left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,7 +12,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
 
-	if (new == NULL)
+	if (parent == NULL)
 		return (NULL);
 
 	new = binary_tree_node(parent, value);
